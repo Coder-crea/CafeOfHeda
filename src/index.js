cb02b388146e2271ea4b373444951539f5748c49
cors_576 <you@example.com>
2025-07-15 14:03:56 +0300

    second

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next";
+// import { Analytics } from "vercel/analytics/next";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
-    <Analytics />
+    {/* <Analytics /> */}
   </BrowserRouter>
 );
